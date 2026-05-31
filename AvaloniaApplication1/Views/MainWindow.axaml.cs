fcd01f0b257a217f6af0459d6ff110705ff193ec
dzonekowy <dzonekowyh2o@gmail.com>
2026-05-31 17:17:33 +0200

    .

--- AvaloniaApplication1/Views/MainWindow.axaml.cs
+++ AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -5,23 +5,6 @@ using Microsoft.Data.SqlClient;
 
 
 
-
-
-// nic nie działa
-// przeszukałem internet, próbowałem instalować pakiety, pakietów nie znajduje ani z visual studio, ani nie mogę ich pobrać za pomocą komendy
-// w przypadku wersji .db nie znajduje w ogóle biblioteki System.Data.SQLite
-// przy instalacji pakietu wyskakują błędy, że nie można zainstalować
-// wszędzie gdzie sprawdzałem, każą je instalować, jednak każde rozwiązanie nie działa
-
-// ostatecznie korzystając z czata dostaję informację, bym upewnił się że mam zainstalowaną paczkę
-
-
-
-
-
-
-
-
 namespace AvaloniaApplication1.Views
 {
     public partial class MainWindow : Window
